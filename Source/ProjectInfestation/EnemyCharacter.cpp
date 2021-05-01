96aa64f0ec241027ad84af29ba99508030df4460
Darian Gratton <dariangratton@gmail.com>
2021-04-30 21:40:08 -0700

    Add base class for enemy swarmer

---
+++ Source/ProjectInfestation/EnemyCharacter.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyCharacter.h"
+
+// Sets default values
+AEnemyCharacter::AEnemyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// Set up collision
+	// TODO: Change so that collision is mapped to specific attack, not just the character's model 
+	GetCapsuleComponent()->OnComponentBeginOverlap.AddDynamic(this, &AEnemyCharacter::OnOverlapBegin);
+	GetCapsuleComponent()->OnComponentEndOverlap.AddDynamic(this, &AEnemyCharacter::OnOverlapEnd);
+}
+
+// Called every frame
+void AEnemyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AEnemyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	// Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AEnemyCharacter::TakeDamage(float damage)
+{
+}
+
+void AEnemyCharacter::OnOverlapBegin(class UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+}
+
+void AEnemyCharacter::OnOverlapEnd(class UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
