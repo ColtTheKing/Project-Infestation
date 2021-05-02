8832aed0cc2a5a11caf2c11949cbba0dc64e9b31
Colt King <king.colt@yahoo.ca>
2021-05-02 00:43:46 -0700

    Made a player controller from scratch that can move, jump, and look around

---
+++ Source/ProjectInfestation/InfestationGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InfestationGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTINFESTATION_API AInfestationGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
