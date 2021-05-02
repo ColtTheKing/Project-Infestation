43bbdfd787e437d6386ecb07a1d2576223bb3b57
Darian Gratton <dariangratton@gmail.com>
2021-05-01 22:12:50 -0700

    Implemented Enemy parent and it's one and only child

---
+++ Source/ProjectInfestation/EnemySwarmer.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyCharacter.h"
+#include "EnemySwarmer.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class PROJECTINFESTATION_API AEnemySwarmer : public AEnemyCharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Default constructor
+	AEnemySwarmer();
+
+protected:
+	// Called on level load
+	void BeginPlay() override;
+
+public:
+	// Called every frame
+	void Tick(float DeltaTime) override;
+
+};
