d0f5eb761b05406e034d6338496902f296563e87
Colt King <king.colt@yahoo.ca>
2021-05-13 01:10:54 -0700

    Fixed an issue with the interact distance being too low, and now destroy the spawner properly when interacting with the baseconsole

--- Source/ProjectInfestation/MyPlayerCharacter.h
+++ Source/ProjectInfestation/MyPlayerCharacter.h
@@ -9,6 +9,7 @@
 #include "Components/InputComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/Controller.h"
+#include <ProjectInfestation/InteractableActor.h>
 
 #include "MyPlayerCharacter.generated.h"
 
@@ -21,24 +22,29 @@ public:
 	// Sets default values for this character's properties
 	AMyPlayerCharacter();
 
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = PlayerSpecs)
+		float interactRange;
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
 		UCameraComponent* playerCamera;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Mesh)
 		USkeletalMeshComponent* playerArms;
 
-	void MoveForward(float axis);
-	void MoveRight(float axis);
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	void MoveForward(float axis);
+	void MoveRight(float axis);
+	void Interact();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:	
+	FHitResult ShootRay(float length);
 };
