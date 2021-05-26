401d44a0109ea30fc87c12823be10c56983d78d6
Darian Gratton <dariangratton@gmail.com>
2021-05-25 22:08:48 -0700

    Added variables for spawn chance to lootbox class

--- Source/ProjectInfestation/Lootbox.cpp
+++ Source/ProjectInfestation/Lootbox.cpp
@@ -3,3 +3,7 @@
 
 #include "Lootbox.h"
 
+ALootbox::ALootbox()
+{
+
+}
--- Source/ProjectInfestation/Lootbox.h
+++ Source/ProjectInfestation/Lootbox.h
@@ -14,4 +14,14 @@ class PROJECTINFESTATION_API ALootbox : public AInteractableActor
 {
 	GENERATED_BODY()
 	
+public:
+	
+	UPROPERTY(EditAnywhere, Category = "Loot", meta = (ClampMin = "0.0", ClampMax = "1.0"))
+		float healthSpawnChance;
+
+	UPROPERTY(EditAnywhere, Category = "Loot", meta = (ClampMin = "0.0", ClampMax = "1.0"))
+		float ammoSpawnChance;
+	
+	ALootbox();
+
 };
