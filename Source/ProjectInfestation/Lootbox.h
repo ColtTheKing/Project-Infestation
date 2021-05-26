7de84705e69795f66a17cb4719fda66987d6aa77
Darian Gratton <dariangratton@gmail.com>
2021-05-25 21:57:40 -0700

    Added Lootbox class and blueprint

---
+++ Source/ProjectInfestation/Lootbox.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractableActor.h"
+#include "Lootbox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTINFESTATION_API ALootbox : public AInteractableActor
+{
+	GENERATED_BODY()
+	
+};
