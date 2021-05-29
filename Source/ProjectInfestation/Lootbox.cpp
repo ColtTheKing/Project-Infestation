255775d473e70d8b69c1b1c42f216d0c1b0f19b8
Darian Gratton <dariangratton@gmail.com>
2021-05-29 01:51:39 -0700

    Implemented logic for basic lootbox

--- Source/ProjectInfestation/Lootbox.cpp
+++ Source/ProjectInfestation/Lootbox.cpp
@@ -7,3 +7,40 @@ ALootbox::ALootbox()
 {
 
 }
+
+void ALootbox::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Get reference to spawn area
+	spawnArea = Cast<UBoxComponent>(GetDefaultSubobjectByName(TEXT("SpawnArea")));
+	opened = false;
+}
+
+void ALootbox::SpawnLoot() 
+{
+	// Check if opened
+	if (opened)
+		return;
+
+	opened = true;
+
+	if (!(FGenericPlatformMath::FRand() < healthSpawnChance))
+		return;
+
+	// Calculate bounding box
+	FTransform localToWorld = FTransform(spawnArea->GetComponentLocation());
+	FBox boundingBox = FBox::BuildAABB(localToWorld.GetLocation(), spawnArea->GetScaledBoxExtent());
+
+	// Calculate random location in bounding box
+	FVector randomLocation;
+	randomLocation = boundingBox.Min;
+	randomLocation.X += FGenericPlatformMath::FRand() * (boundingBox.Max.X - boundingBox.Min.X);
+	randomLocation.Y += FGenericPlatformMath::FRand() * (boundingBox.Max.Y - boundingBox.Min.Y);
+	randomLocation.Z += (boundingBox.Max.Z - boundingBox.Min.Z);
+
+	// Spawn a enemy
+	FRotator spawnRotation = FRotator(0.0f, 0.0f, 0.0f);
+	AActor* spawnedEnemy = GetWorld()->SpawnActor(healthBP, &randomLocation, &spawnRotation);
+
+}
--- Source/ProjectInfestation/Lootbox.h
+++ Source/ProjectInfestation/Lootbox.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "InteractableActor.h"
+#include "Components/BoxComponent.h"
+
 #include "Lootbox.generated.h"
 
 /**
@@ -13,15 +15,28 @@ UCLASS()
 class PROJECTINFESTATION_API ALootbox : public AInteractableActor
 {
 	GENERATED_BODY()
-	
+
+	UBoxComponent* spawnArea;
+
+	bool opened;
+
 public:
+	// Default constuctor
+	ALootbox();
 	
-	UPROPERTY(EditAnywhere, Category = "Loot", meta = (ClampMin = "0.0", ClampMax = "1.0"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Loot", meta = (ClampMin = "0.0", ClampMax = "1.0"))
 		float healthSpawnChance;
 
-	UPROPERTY(EditAnywhere, Category = "Loot", meta = (ClampMin = "0.0", ClampMax = "1.0"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Loot", meta = (ClampMin = "0.0", ClampMax = "1.0"))
 		float ammoSpawnChance;
-	
-	ALootbox();
 
+	UPROPERTY(EditAnywhere, Category = "Loot")
+		TSubclassOf<AActor> healthBP;
+
+	UFUNCTION(BlueprintCallable)
+		void SpawnLoot();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 };
