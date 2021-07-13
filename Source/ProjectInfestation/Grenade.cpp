61ef0dfdc1d70f60829959e2150004b796e7e9d4
Darian Gratton <dariangratton@gmail.com>
2021-07-13 15:27:28 -0700

    Added Grenade classes

---
+++ Source/ProjectInfestation/Grenade.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Grenade.h"
+
+AGrenade::AGrenade() : Super()
+{
+
+}
+
+void AGrenade::Tick(float DeltaTime) 
+{
+
+}
+
+void AGrenade::UseWeapon(AActor* actor)
+{
+
+}
+
+void AGrenade::ConsumeAmmo(int ammo) 
+{
+
+}
+
+void AGrenade::ThrowGrenade() 
+{
+
+}
---
+++ Source/ProjectInfestation/Grenade.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "Grenade.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTINFESTATION_API AGrenade : public AWeapon
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AGrenade();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Attack")
+		void UseWeapon(AActor* actor) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Ammo")
+		void ConsumeAmmo(int ammo) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+	// Handle throwing logic
+	void ThrowGrenade();
+
+};
