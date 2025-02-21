d9bbacaf4e0d4abdec373f70cc858c33862c32e0
Colt King <king.colt@yahoo.ca>
2025-02-20 21:46:41 -0800

    Added a weapon wheel ui screen that detects where you click (apparently i forgot which feature i was working on).

---
+++ Source/ProjectInfestation/InfestUtilities.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InfestUtilities.h"
+
+bool UInfestUtilities::GetNormalisedMousePositionInGeometry(UObject* WorldContextObject, FGeometry Geometry, FVector2D& Position)
+{
+	FVector2D pixelPosition;
+	FVector2D viewportMinPosition;
+	FVector2D viewportMaxPosition;
+
+	// Get the top left and bottom right viewport positions
+	USlateBlueprintLibrary::LocalToViewport(WorldContextObject, Geometry, FVector2D(0, 0), pixelPosition, viewportMinPosition);
+	USlateBlueprintLibrary::LocalToViewport(WorldContextObject, Geometry, Geometry.GetLocalSize(), pixelPosition, viewportMaxPosition);
+
+	// Get the mouse's current position in the viewport
+	FVector2D mousePosition = UWidgetLayoutLibrary::GetMousePositionOnViewport(WorldContextObject);
+
+	// Determine where the mouse is relative to the widget geometry
+	Position = (mousePosition - viewportMinPosition) / (viewportMaxPosition - viewportMinPosition);
+
+	// Calculate if the mouse is inside the geometry or not
+	return FMath::Min(Position.X, Position.Y) >= 0.f && FMath::Max(Position.X, Position.Y) <= 1.f;
+}
---
+++ Source/ProjectInfestation/InfestUtilities.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+
+#include "Layout/Geometry.h" 
+#include "Blueprint/SlateBlueprintLibrary.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
+
+#include "InfestUtilities.generated.h"
+
+UCLASS()
+class PROJECTINFESTATION_API UInfestUtilities : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = UI)
+		static bool GetNormalisedMousePositionInGeometry(UObject* WorldContextObject, FGeometry Geometry, FVector2D& Position);
+};
