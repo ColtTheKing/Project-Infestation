855e510dd4826d15d0bbf63f90f535170307b164
Darian Gratton <25462660+DarianGratton@users.noreply.github.com>
2025-05-08 21:16:35 -0700

    Added initial variables and functions for parent AIBehaviour class

--- Source/ProjectInfestation/AI/Behaviours/AIBehaviour.cpp
+++ Source/ProjectInfestation/AI/Behaviours/AIBehaviour.cpp
@@ -3,3 +3,35 @@
 
 #include "AIBehaviour.h"
 
+bool UAIBehaviour::IsValidSelectionOption() const
+{
+	return !IsCoolingDown() && AreStartingConditionsMet();
+}
+
+bool UAIBehaviour::IsCoolingDown() const
+{
+	return false;
+}
+
+bool UAIBehaviour::AreStartingConditionsMet() const
+{
+	return false;
+}
+
+float UAIBehaviour::GetSelectionScore() const
+{
+	return 0.0f;
+}
+
+void UAIBehaviour::StartBehaviour() const
+{
+}
+
+void UAIBehaviour::StopBehaviour() const
+{
+}
+
+void UAIBehaviour::GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const
+{
+	TagContainer.AddTag(behaviourType);
+}
--- Source/ProjectInfestation/AI/Behaviours/AIBehaviour.h
+++ Source/ProjectInfestation/AI/Behaviours/AIBehaviour.h
@@ -4,14 +4,56 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "GameplayTagAssetInterface.h"
 #include "AIBehaviour.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class PROJECTINFESTATION_API UAIBehaviour : public UObject
+class PROJECTINFESTATION_API UAIBehaviour : public UObject, public IGameplayTagAssetInterface
 {
 	GENERATED_BODY()
 	
+public:
+
+	// Can this behaviour be started i.e. is it valid option for selection?
+	UFUNCTION(BlueprintCallable)
+		bool IsValidSelectionOption() const;
+
+	// Is the behaviour currently colling down?
+	UFUNCTION(BlueprintCallable)
+		bool IsCoolingDown() const;
+
+	// Are the conditions valid for this behaviour to be started
+	UFUNCTION(BlueprintCallable)
+		virtual bool AreStartingConditionsMet() const;
+
+	// Calculates a score used of behaviour selection when we have multiple options
+	UFUNCTION(BlueprintCallable)
+		virtual float GetSelectionScore() const;
+
+	UFUNCTION(BlueprintCallable)
+		void StartBehaviour() const;
+
+	UFUNCTION(BlueprintCallable)
+		void StopBehaviour() const;
+
+	// From IGameplayTagAssetInterface
+	virtual void GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI Behaviour")
+		FString behaviourName;
+
+	// Behaviour tree for behaviour
+	UPROPERTY(EditAnywhere, Category = "AI Behaviour")
+		UBehaviorTree* behaviorTree;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI Behaviour")
+		FGameplayTag behaviourType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI Behaviour")
+		bool isInterruptible;
 };
