0597f3a28a7c304bbc5c964799119780090838fc
Darian Gratton <25462660+DarianGratton@users.noreply.github.com>
2025-05-08 20:38:12 -0700

    Added initial parent behaviour

---
+++ Source/ProjectInfestation/AI/Behaviours/AIBehaviour.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "AIBehaviour.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTINFESTATION_API UAIBehaviour : public UObject
+{
+	GENERATED_BODY()
+	
+};
