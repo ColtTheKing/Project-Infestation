7c0221970a8a6aefa3e4b23421b946f92ba0943b
Darian Gratton <25462660+DarianGratton@users.noreply.github.com>
2026-02-06 20:32:11 -0800

    Add AI behavior group class

---
+++ Source/ProjectInfestation/AI/AIBehaviorGroup.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+
+#include "AIBehavior.h"
+#include "Objectives/AIObjective.h"
+
+#include "AIBehaviorGroup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTINFESTATION_API UAIBehaviorGroup : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UAIBehaviorGroup();
+
+	// Are the conditions valid for this behavior group
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+		bool AreStartingConditionsMet(AActor* aiActor, const TArray<UAIObjective*>& availableObjectives);
+
+protected:
+	// Name of the behavior group.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Behavior Group")
+		FString behaviorName;
+
+	// List of behaviors in group.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Behavior Group")
+		TArray<TObjectPtr<UAIBehavior>> behaviors;
+
+	// List of behavior groups in group.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Behavior Group")
+		TArray<TObjectPtr<UAIBehaviorGroup>> behaviorGroups;
+
+	// Cooldown time between when this behavior group can be selected again.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Behavior Group")
+		double cooldownTime;
+};
